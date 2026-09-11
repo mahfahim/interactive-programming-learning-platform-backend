7fac97b0ab1dc2d1152d7cae38587b4726dbe3f5
Md. Abdul Hai Fahim <mahfahim51@gmail.com>
2026-09-12 05:54:16 +0600

    fix(assignment): update prisma import for esm compatibility

--- src/app/modules/assignment/assignment.service.ts
+++ src/app/modules/assignment/assignment.service.ts
@@ -1,4 +1,4 @@
-import { Prisma } from "@prisma/client";
+import { Prisma } from "../../../generated/prisma/client";
 import httpStatus from "http-status";
 import { prisma } from "../../lib/prisma";
 import { AppError } from "../../utils/AppError";
